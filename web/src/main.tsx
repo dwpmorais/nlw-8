fb3b8b0d9ded761644fdcc19faa88fcef030dfb7
Douglas <douglas.morais@driven.cx>
2022-05-04 23:14:56 -0300

    feature: add tailwind / dark mode

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import './css/global.css'
+import { ThemeProvider } from './components/ThemeContext'
+import Background from './components/Background'
+import Toggle from './components/ThemeToggle'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <Background>
+        <div className="absolute right-0 top-0 mr-4 mt-4 md:mr-6 md:mt-6">
+          <Toggle />
+        </div>
+        <App />
+      </Background>
+    </ThemeProvider>
   </React.StrictMode>
 )
